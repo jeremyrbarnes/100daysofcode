c864bdfc897ed23032f1743c6a4cf5f9c5424ddf
Jeremy Barnes <jeremy.r.barnes@gmail.com>
2023-02-27 10:15:56 -0500

    updates to day 5

--- day_5_jane_street.py
+++ day_5_jane_street.py
@@ -11,3 +11,21 @@ def cons(a, b):
     return pair
 Implement car and cdr.
 '''
+
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(p):
+    return p.__closure__[0].cell_contents
+
+
+def cdr(p):
+    return p.__closure__[1].cell_contents
+
+
+print(car(cons(3, 4)))
+print(cdr(cons(3, 4)))
